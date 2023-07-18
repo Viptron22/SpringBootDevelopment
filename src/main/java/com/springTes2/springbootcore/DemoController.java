5a85ba7171bad08fefa60b03dc34a105eafb1b42
viptron22 <vidhujabade@protonmail.com>
2023-07-18 11:36:37 +0530

    check

--- src/main/java/com/springTes2/springbootcore/DemoController.java
+++ src/main/java/com/springTes2/springbootcore/DemoController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+
+    //Constructor injection
     @Autowired
     public DemoController(Coach newCoach)
     {
