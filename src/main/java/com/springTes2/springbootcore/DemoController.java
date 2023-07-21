f288e0742907de5d849ee70dc489e8f215db3095
viptron22 <vidhujabade@protonmail.com>
2023-07-21 15:53:03 +0530

    d

--- src/main/java/com/springTes2/springbootcore/DemoController.java
+++ src/main/java/com/springTes2/springbootcore/DemoController.java
@@ -1,12 +1,12 @@
 package com.springTes2.springbootcore;
 
-<<<<<<< HEAD
+
 import com.springTes2.springbootcore.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-=======
+
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 0cb3351bb9edb3c079a876e7eff1b9bc615ea3a0
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,16 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
-<<<<<<< HEAD
+
 
     //Constructor injection
     //changes in branch
     @Autowired
     public DemoController(@Qualifier("cricketCoach") Coach newCoach)
-=======
+
     @Autowired
     public DemoController(Coach newCoach)
->>>>>>> 0cb3351bb9edb3c079a876e7eff1b9bc615ea3a0
+
     {
         myCoach=newCoach;
     }
